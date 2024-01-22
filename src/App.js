620013191cb6f5021482fb7585ffbac4cbcfbd53
Po Jui Juan <bray851011@gmail.com>
2024-01-22 11:07:23 -0500

    Add search box

--- src/App.js
+++ src/App.js
@@ -13,21 +13,37 @@ class App extends Component{
 
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users) => this.setState(
-      () => {
-        return {monsters: users}
-      }
-      ,
-      () => {
-        console.log(this.state);
-      }
-    ))
+      .then((response) => response.json())
+      .then((users) => this.setState(
+        () => {
+          return {monsters: users}
+        }
+        ,
+        () => {
+          console.log(this.state);
+        }
+      ))
   }
 
   render(){
     return (
       <div className="App">
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monster' 
+          onChange={(event) => {
+            const searchString = event.target.value.toLocaleLowerCase();            
+            
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            })
+            
+            this.setState(() => {
+              return {monsters: filteredMonsters}
+            })
+          }}
+        />
         {
           this.state.monsters.map((monsters) => {
             return (
