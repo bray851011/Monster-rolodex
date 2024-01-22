e4c288470d663977ce1463dfd6314e2d63a4d298
Po Jui Juan <bray851011@gmail.com>
2024-01-22 08:15:29 -0500

    Add constructor

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      name: "Ray",
+
+    }
+  }
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>{this.state.name}</p>
         </header>
       </div>
     );
